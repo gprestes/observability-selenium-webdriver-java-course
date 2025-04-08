d82764697a6cddb1529885ee7022b9d3b01cb3a3
Guilherme Heuser Prestes <gprestes@users.noreply.github.com>
2025-04-08 11:19:35 +0200

    Update EventReporter.java

--- webdriver_java/src/main/java/utils/EventReporter.java
+++ webdriver_java/src/main/java/utils/EventReporter.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.events.WebDriverListener;
 
-public class EventReporter implements WebDriverEventListener {
+public class EventReporter implements WebDriverListener {
     @Override
     public void beforeAlertAccept(WebDriver webDriver) {
 
