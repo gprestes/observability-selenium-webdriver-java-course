1d7755233f3a541d6ec6e7045d933d183e3a90f9
Guilherme Heuser Prestes <gprestes@users.noreply.github.com>
2025-04-08 11:17:30 +0200

    Update EventReporter.java

--- webdriver_java/src/main/java/utils/EventReporter.java
+++ webdriver_java/src/main/java/utils/EventReporter.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.events.WebDriverEventListener;
+import org.openqa.selenium.support.events.WebDriverListener;
 
 public class EventReporter implements WebDriverEventListener {
     @Override
