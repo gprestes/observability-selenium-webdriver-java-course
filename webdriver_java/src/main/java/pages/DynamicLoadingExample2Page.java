2abb2db5b7b455026d3f4d9236dc2354c13f7e98
Guilherme Heuser Prestes <gprestes@users.noreply.github.com>
2025-04-08 11:35:22 +0200

    Update DynamicLoadingExample2Page.java

--- webdriver_java/src/main/java/pages/DynamicLoadingExample2Page.java
+++ webdriver_java/src/main/java/pages/DynamicLoadingExample2Page.java
@@ -17,7 +17,7 @@ public class DynamicLoadingExample2Page {
 
     public void clickStart() {
         driver.findElement(startButton).click();
-        WebDriverWait wait = new WebDriverWait(driver, 5);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         wait.until(ExpectedConditions.presenceOfElementLocated(loadedText));
     }
 
